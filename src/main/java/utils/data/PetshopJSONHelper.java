104f2b0dfe70c0d045eb7f90bad801e557b4d16b
dmitriiyashkin <dmitriiyashkin@gmail.com>
2020-06-10 18:36:01 +0300

    test add PetshopJSONHelper class that provide info for creating new user by generateJSONForCreateNewUser() method

--- src/main/java/utils/data/PetshopJSONHelper.java
+++ src/main/java/utils/data/PetshopJSONHelper.java
@@ -1,4 +1,20 @@
-package utils;
+package utils.data;
+
+
+import org.json.simple.JSONObject;
+import utils.api.Authorization;
 
 public class PetshopJSONHelper {
+    public static String generateJSONForCreateNewUser() {
+        JSONObject credentials = new JSONObject();
+        credentials.put("id", "0");
+        credentials.put("username", Authorization.username);
+        credentials.put("firstName", Authorization.firstName);
+        credentials.put("lastName", Authorization.lastName);
+        credentials.put("email", Authorization.email);
+        credentials.put("password", Authorization.password);
+        credentials.put("phone", Authorization.phone);
+        credentials.put("userStatus", "0");
+        return credentials.toJSONString();
+    }
 }
--- src/test/java/pettest_v2/PetWithJSONObjectTest.java
+++ src/test/java/pettest_v2/PetWithJSONObjectTest.java
@@ -1,4 +1,65 @@
 package pettest_v2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.*;
+import utils.api.Authorization;
+import utils.data.PetshopJSONHelper;
+
+import static io.restassured.RestAssured.given;
+
 public class PetWithJSONObjectTest {
+    private static ValidatableResponse response;
+    static {
+        RestAssured.baseURI = Authorization.BASE_URL;
+    }
+
+    @BeforeClass
+    public static void createUser() {
+        String newUser = PetshopJSONHelper.generateJSONForCreateNewUser();
+        given().
+                header("Content-Type", "application/json").
+                body(newUser).
+                when().
+                post("/user").
+                then().
+                statusCode(200).contentType(ContentType.JSON);
+    }
+
+    @BeforeTest
+    public void login() {
+        response = given().
+                header("Content-Type", "application/json").
+                when().
+                get("/user/login?username=YaName&password=12345").
+                then().
+                statusCode(200).contentType(ContentType.JSON);
+    }
+
+
+    @Test
+    public void someTest(){
+
+    }
+
+    @AfterTest
+    public void logout() {
+        response = given().
+                header("Content-Type", "application/json").
+                when().
+                get("/user/logout").
+                then().
+                statusCode(200).contentType(ContentType.JSON);
+    }
+
+    @AfterClass
+    public static void deleteUser() {
+        response = given().
+                header("Content-Type", "application/json").
+                when().
+                delete("/user/YaName").
+                then().
+                statusCode(200).contentType(ContentType.JSON);
+    }
 }
