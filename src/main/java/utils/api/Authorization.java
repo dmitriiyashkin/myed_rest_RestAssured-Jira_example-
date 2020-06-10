cbfd1c9dfefe972b6d1f7958f05cfdb0fa259744
dmitriiyashkin <dmitriiyashkin@gmail.com>
2020-06-10 18:28:54 +0300

    test add Authorization class for keeping info about user's data

---
+++ src/main/java/utils/api/Authorization.java
@@ -0,0 +1,11 @@
+package utils.api;
+
+public class Authorization {
+    public static String BASE_URL = "https://petstore.swagger.io/v2";
+    public static String username = "YaUser";
+    public static String email = "ya@gmail.com";
+    public static String phone = "8800200600";
+    public static String firstName = "Ivan";
+    public static String lastName = "Ivanov";
+    public static Integer password = 12345;
+}
